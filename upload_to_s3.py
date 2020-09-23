5cb28c3a89416651955fe6555ddaf804bc7a95e3
Tilo Himmelsbach <dertilo@gmail.com>
2020-09-23 18:20:03 +0200

    unzip tar-zip

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -1,12 +1,13 @@
 from sagemaker.s3 import S3Uploader
+from util import data_io
 from util.util_methods import exec_command
 import os
 
 if __name__ == '__main__':
     local_path = f"{os.environ['HOME']}/data"
-    file_to_upload = f"{local_path}/wikitext-2-raw-v1.zip"
-    if not os.path.isfile(file_to_upload):
-        os.system(f"cd {local_path} && wget --trust-server-names https://s3.amazonaws.com/research.metamind.io/wikitext/{file_to_upload}")
-
+    data_io.download_data("https://s3.amazonaws.com/research.metamind.io/wikitext","wikitext-2-raw-v1.zip",local_path,unzip_it=True,remove_zipped=True)
+    folder_name = "wikitext-2-raw-v1"
+    file_to_upload = f"/tmp/{folder_name}.tar.gz"
+    exec_command(f"cd {local_path} && tar -czvf {file_to_upload} {folder_name}")
     s3_prefix = "s3://tilos-ml-bucket/wikitext-2-raw-v1"
     S3Uploader.upload(file_to_upload, f"{s3_prefix}")
