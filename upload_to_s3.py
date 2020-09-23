e35d0c5be556668485ec28ce20e1940253dedee5
Tilo Himmelsbach <dertilo@gmail.com>
2020-09-23 17:07:44 +0200

    upload_to_s3.py

---
+++ upload_to_s3.py
@@ -0,0 +1,12 @@
+from sagemaker.s3 import S3Uploader
+from util.util_methods import exec_command
+import os
+
+if __name__ == '__main__':
+    local_path = f"{os.environ['HOME']}/data"
+    file_to_upload = f"{local_path}/wikitext-2-raw-v1.zip"
+    if not os.path.isfile(file_to_upload):
+        os.system(f"cd {local_path} && wget --trust-server-names https://s3.amazonaws.com/research.metamind.io/wikitext/{file_to_upload}")
+
+    s3_prefix = "s3://tilos-ml-bucket"
+    S3Uploader.upload(file_to_upload, f"{s3_prefix}")
