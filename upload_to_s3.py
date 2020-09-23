a6032266690f911134f0ab569cbc791447cfd24d
Tilo Himmelsbach <dertilo@gmail.com>
2020-09-23 17:13:34 +0200

    sagemaker_lm

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     if not os.path.isfile(file_to_upload):
         os.system(f"cd {local_path} && wget --trust-server-names https://s3.amazonaws.com/research.metamind.io/wikitext/{file_to_upload}")
 
-    s3_prefix = "s3://tilos-ml-bucket"
+    s3_prefix = "s3://tilos-ml-bucket/wikitext-2-raw-v1"
     S3Uploader.upload(file_to_upload, f"{s3_prefix}")
